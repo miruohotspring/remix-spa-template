5d0e6457ef447c813270577d191d8cef808addf8
miruohotspring <miruohotspring@gmail.com>
2024-08-27 06:41:01 +0900

    fix everything

--- app/root.tsx
+++ app/root.tsx
@@ -10,7 +10,7 @@ import './globals.css';
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
