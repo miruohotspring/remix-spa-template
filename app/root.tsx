9f8a94695488d826c355e0fe5021e408372b0a3e
miruohotspring <miruohotspring@gmail.com>
2024-08-27 05:48:53 +0900

    add biome setting

--- app/root.tsx
+++ app/root.tsx
@@ -6,6 +6,8 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
+import './globals.css'
+
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,38 +1,41 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { MetaFunction } from '@remix-run/node';
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix SPA" },
-    { name: "description", content: "Welcome to Remix (SPA Mode)!" },
+    { title: 'New Remix SPA' },
+    { name: 'description', content: 'Welcome to Remix!' },
   ];
 };
 
 export default function Index() {
   return (
-    <div className="font-sans p-4">
-      <h1 className="text-3xl">Welcome to Remix (SPA Mode)</h1>
-      <ul className="list-disc mt-4 pl-6 space-y-2">
+    <main className="container prose py-8">
+      <h1>Welcome to Remix</h1>
+      <ul>
         <li>
           <a
-            className="text-blue-700 underline visited:text-purple-900"
             target="_blank"
-            href="https://remix.run/guides/spa-mode"
+            href="https://remix.run/tutorials/blog"
             rel="noreferrer"
           >
-            SPA Mode Guide
+            15m Quickstart Blog Tutorial
           </a>
         </li>
         <li>
           <a
-            className="text-blue-700 underline visited:text-purple-900"
             target="_blank"
-            href="https://remix.run/docs"
+            href="https://remix.run/tutorials/jokes"
             rel="noreferrer"
           >
+            Deep Dive Jokes App Tutorial
+          </a>
+        </li>
+        <li>
+          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
             Remix Docs
           </a>
         </li>
       </ul>
-    </div>
+    </main>
   );
 }
